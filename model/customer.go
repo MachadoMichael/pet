654299b68e01373685d54eb4857309bb5fa1eec9
Michael <machado.m3@icloud.com>
2024-10-06 08:08:40 -0300

    feat: add model.customer

--- model/customer.go
+++ model/customer.go
@@ -1,1 +1,17 @@
 package model
+
+import (
+	"github.com/oklog/ulid"
+)
+
+type Customer struct {
+	ID                 ulid.ULID `json:"id"`
+	Name               string    `json:"name"`
+	Cpf                string    `json:"cpf"`
+	Phone              string    `json:"phone"`
+	Email              string    `json:"email"`
+	Address            string    `json:"address"`
+	Photo              string    `json:"photo"`
+	CardNumber         int       `json:"card_number"`
+	SecurityCardNumber int       `json:"security_card_number"`
+}
