831afcff94af1db99a8336dfbc28966ba78abf95
Michael <machado.m3@icloud.com>
2024-10-06 08:14:21 -0300

    feat: add model.animalCarer

---
+++ model/animalCarer.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"github.com/oklog/ulid"
+)
+
+type AnimalCarer struct {
+	ID               ulid.ULID `json:"id"`
+	Name             string    `json:"name"`
+	Address          string    `json:"address"`
+	Phone            string    `json:"phone"`
+	Email            string    `json:"email"`
+	Description      string    `json:"description"`
+	Tags             string    `json:"tags"`
+	ProofOfResidence string    `json:"proof_of_residence"`
+	Photos           [5]string `json:"photos"`
+	PixKey           string    `json:"pix_key"`
+	Age              int       `json:"age"`
+}
