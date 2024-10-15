14a65d88bedef4ac2d960183dff582a14e5757cb
machadomichael <machado.m3@icloud.com>
2024-10-15 00:23:46 +0000

    test: add test to open db

--- infra/database/db_test.go
+++ infra/database/db_test.go
@@ -1,39 +1,41 @@
 package database
 
 import (
+	"database/sql"
 	"testing"
 
-	"github.com/MachadoMichael/pet/config"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestInit(t *testing.T) {
-	// Test successful initialization
-	db, err := Init()
-	assert.NoError(t, err)
-	assert.NotNil(t, db)
-
-	// Test failed initialization (e.g. invalid connection string)
-	invalidConnStr := "invalid://connection:string"
-	config.Variables.ConnStrDb = invalidConnStr
-	db, err = Init()
-	assert.Error(t, err)
-	assert.Nil(t, db)
+type MockDB struct {
+	mock.Mock
 }
 
-//
-// func TestMigration(t *testing.T) {
-// 	// Test successful migration
-// 	db, err := Init()
-// 	assert.NoError(t, err)
-// 	assert.NotNil(t, db)
-//
-// 	err = Migration(db)
-// 	assert.NoError(t, err)
-//
-// 	// Test failed migration (e.g. invalid migration file)
-// 	invalidMigrationFile := "invalid_migration_file.sql"
-// 	config.Variables.MigrationsPath = invalidMigrationFile
-// 	err = Migration(db)
-// 	assert.Error(t, err)
-// }
+func (m *MockDB) Open(database, connStr string) (*sql.DB, error) {
+	args := m.Called(database, connStr)
+	return args.Get(0).(*sql.DB), nil
+}
+
+func TestOpenDB(t *testing.T) {
+	// Create an instance of MockDB
+	mockDB := new(MockDB)
+
+	// Define expected arguments and return values
+	database := "test_db"
+	connStr := "test_connection_string"
+	expectedDB := &sql.DB{} // This can be a real DB or a mock object, as needed
+
+	// Set up expectations on the mock
+	mockDB.On("Open", database, connStr).Return(expectedDB, nil)
+
+	// Call the Open method
+	db, err := mockDB.Open(database, connStr)
+
+	// Assert that the return values are as expected
+	assert.NoError(t, err)
+	assert.Equal(t, expectedDB, db)
+
+	// Assert that the expectations were met
+	mockDB.AssertExpectations(t)
+}
